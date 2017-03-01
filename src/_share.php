1b51bc921d9455027b3f409df3c2083c8aee147a
Duarte Henriques <duarte.henriques@gmail.com>
2017-03-01 16:32:16 +0000

    Add more translations

--- src/_share.php
+++ src/_share.php
@@ -1,10 +1,10 @@
 <section id="share" class="bg-gray z-index-9">
   <div class="content">
     <p class="txt-center" style="padding-top: 80px;">
-      <b>Enjoy a water sensible diet.</b>   
+      <b data-i18n>Enjoy a water sensible diet.</b>
     </p>
     <p class="txt-center" style="font-size: 58px;padding: 20px 0px 120px 0px; line-height: 85px;">
-      <b>Start today and<br/>spread the message!</b>
+      <b data-i18n>Start today and<br>spread the message!</b>
     </p>
     <?php include_partial('addthis'); ?>
   </div>
