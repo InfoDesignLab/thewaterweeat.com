20015d9f13b253a65524669a034cf5d8106e1585
Duarte Henriques <duarte.henriques@gmail.com>
2017-03-01 23:43:18 +0000

    Simplify footer

--- src/_authors.php
+++ src/_authors.php
@@ -6,19 +6,15 @@
     </a>
   </b>
   <br/><br/>
-  Concept, design, article, virtual water ambassador: <b><a href="http://www.angelamorelli.com" target="_blank">Angela Morelli</a></b><br/>
-  Sources, scientific research, data: <b><a href="http://www.waterfootprint.org" target="_blank">Water Footprint Network</a>, <a href=" http://www.amazon.co.uk/Virtual-Water-Tackling-Precious-Resource/dp/1845119843" target="_blank">Virtual Water by Tony Allan</a></b><br/>
-  UI/UX design and development, patience medalist: <b><a href="http://basili.co" target="_blank">Basilico Interactive</a></b><br/>
-  Design advisor, Jiminy cricket, mental supporter: <b><a href="http://twitter.com/tomhal99" target="_blank">Tom Halsør</a></b><br/>
-  Editorial advisor, water guru: <b><a href="http://www.ibtauris.com/Geography%20and%20Social%20Sciences.aspx" target="_blank">David Stonestreet</a></b>
-  <br/>
+  By <b><a href="https://infodesignlab.com/" target="_blank">InfoDesignLab</a></b><br/>
+  Sources: <b><a href="http://www.waterfootprint.org" target="_blank">Water Footprint Network</a>, <a href=" http://www.amazon.co.uk/Virtual-Water-Tackling-Precious-Resource/dp/1845119843" target="_blank">Virtual Water by Tony Allan</a></b><br/>
   <br/>
   <?php $browser = getBrowser(); ?>
   <?php $is_old = $browser['name'] == 'msie' && $browser['version'] < 9; ?>
   <?php if ($is_old || isMobileDevice()): ?>
     <span style="color: #3e4c3e; font-size: 13px;">For a full-animated version use Chrome, Firefox, Safari. You will get a static version on Internet Explorer 9-8-7, Opera, iPad.</span>
-    <br/>  
-    <br/>  
+    <br/>
+    <br/>
   <?php endif; ?>
 <div style="color: #fff; font-size: 12px;">
   <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png" /></a><br />This work by <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.angelamorelli.com" property="cc:attributionName" rel="cc:attributionURL">Angela Morelli</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.en_US">Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License</a>.<br />Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="http://www.angelamorelli.com" rel="dct:source">www.angelamorelli.com</a>.
