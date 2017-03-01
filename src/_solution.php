366b7930893e2ae4f59d11923613009b0806cc47
Duarte Henriques <duarte.henriques@gmail.com>
2017-03-01 23:51:52 +0000

    Change style of "can we help? we can!"

--- src/_solution.php
+++ src/_solution.php
@@ -2,12 +2,12 @@
   <div class="wave-top"></div>
   <div class="container bg-blue">
     <div class="content">
-      <p class="txt-uppercase txt-center"><b data-i18n>so now we know:</b></p>
+      <p class="txt-center"><b data-i18n>so now we know:</b></p>
       <br/>
       <p data-i18n class="txt-justify">most of the water we use <b>- 92 % of it -</b> is used in food<br>production. Most of this water is managed by the world’s<br>farmers. With the help of science and technology they have<br>performed greater and greater miracles in improving water<br>productivity – in getting more crops per drop.</p>
       <br/><br/><br/><br/>
-      <p class="txt-uppercase txt-center"><b data-i18n>can we help?</b></p>
-      <h1 data-i18n class="txt-uppercase txt-center" style="margin: 0px; font-size: 105px;">we can!</h1>
+      <p class="txt-center"><b data-i18n>can we help?</b></p>
+      <p class="txt-uppercase txt-center"><b data-i18n>we can!</b></p>
     </div>
   </div>
   <div class="wave-bottom"></div>
