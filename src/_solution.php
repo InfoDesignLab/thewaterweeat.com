dd8888162282eec7e2cff4fc4b6143f164281d4c
Duarte Henriques <duarte.henriques@gmail.com>
2017-03-02 16:43:16 +0000

    Capitalize first word of a couple of sentences in solution section

--- src/_solution.php
+++ src/_solution.php
@@ -2,11 +2,11 @@
   <div class="wave-top"></div>
   <div class="container bg-blue">
     <div class="content">
-      <p class="txt-center"><b data-i18n>so now we know:</b></p>
+      <p class="txt-center"><b data-i18n>So now we know:</b></p>
       <br/>
       <p data-i18n class="txt-justify">most of the water we use <b>- 92 % of it -</b> is used in food<br>production. Most of this water is managed by the world’s<br>farmers. With the help of science and technology they have<br>performed greater and greater miracles in improving water<br>productivity – in getting more crops per drop.</p>
       <br/><br/><br/><br/>
-      <p class="txt-center"><b data-i18n>can we help?</b></p>
+      <p class="txt-center"><b data-i18n>Can we help?</b></p>
       <p class="txt-uppercase txt-center"><b data-i18n>we can!</b></p>
     </div>
   </div>
