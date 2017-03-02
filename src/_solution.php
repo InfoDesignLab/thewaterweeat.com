e6c1b25907be7ba3985387f0c2e8023c4576b3d2
Duarte Henriques <duarte.henriques@gmail.com>
2017-03-02 16:53:52 +0000

    Make "we can!" more salient

--- src/_solution.php
+++ src/_solution.php
@@ -7,7 +7,7 @@
       <p data-i18n class="txt-justify">most of the water we use <b>- 92 % of it -</b> is used in food<br>production. Most of this water is managed by the world’s<br>farmers. With the help of science and technology they have<br>performed greater and greater miracles in improving water<br>productivity – in getting more crops per drop.</p>
       <br/><br/><br/><br/>
       <p class="txt-center"><b data-i18n>Can we help?</b></p>
-      <p class="txt-uppercase txt-center"><b data-i18n>we can!</b></p>
+      <p class="txt-uppercase txt-center" style="font-size: 4em;"><b data-i18n>we can!</b></p>
     </div>
   </div>
   <div class="wave-bottom"></div>
